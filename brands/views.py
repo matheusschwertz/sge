cc619dfd4521bfbf01d09ee9ce291ba7ca617cd2
matheusschwertz <matheusschwertz@gmail.com>
2025-04-03 13:10:06 -0300

    new feature

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from . import models
 
 
@@ -6,3 +7,19 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = ...
+    success_url = reverse_lazy('brand_list')
+
